6e1706015b8ec44b3a2584f3a89bbc6d8d9adbf9
Ed Scrimaglia <edgardo.scrimaglia@gmail.com>
2025-03-12 13:53:15 -0300

    refactoring __main__.py

--- cli_automation/__main__.py
+++ cli_automation/__main__.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 from .main import app
+
 app(prog_name="cla")
 
-cla_html = Path("cla.html") 
-if cla_html.exists():
-    data = cla_html.read_text()
-    index_html = Path("index.html")
-    index_html.write_text(data)
+index_html = Path("index.html")
+if not index_html.exists():
+    cla_html = Path("cla.html") 
+    if cla_html.exists():
+        data = cla_html.read_text()
+        index_html.write_text(data)
 

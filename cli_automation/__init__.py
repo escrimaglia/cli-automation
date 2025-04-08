81438e63f69902d84562361058179bebf9e605c3
Ed Scrimaglia <edgardo.scrimaglia@gmail.com>
2025-04-08 02:09:35 -0300

    version 1.6.1

--- cli_automation/__init__.py
+++ cli_automation/__init__.py
@@ -8,7 +8,7 @@ DATA = {
     "tunnel": False,
     "app": "cla",
 }
-__version__ = "1.5.3 - XXI - By Ed Scrimaglia"
+__version__ = "1.6.1 - XXI - By Ed Scrimaglia"
 
 class ClaConfig():
     def __init__(self):
